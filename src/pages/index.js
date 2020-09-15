494ba1dc3120b21027452bd0172d4749c7ced427
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-15 19:32:28 -0300

    Working on images (WIP)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Image from "../components/image"
+import ImagePlayStore from "../components/playStoreImage"
+import ImageAppStore from "../components/appStoreImage"
+import Logo from "../components/logo"
+
 import SEO from "../components/seo"
 import "../components/layout.css"
 
 const IndexPage = () => (
   <>
     <SEO title="Crypto Tracker" />
-
     <div className="container text-align">
       <div className="row h-100">
         <div className="col-lg-6 my-auto">
-          <img src="" alt="Logo" />
+          <Logo />
           <h1>Cryto Tracker</h1>
           <p>
             Conocé las distintas cotizaciones de la divisa norteamericana en la
             Argentina de manera simple, elegante y efectiva.
           </p>
-          <div>
-            <button>App Store</button>
-            <button>Play Store</button>
+          <div className="store-buttons">
+            <ImagePlayStore />
+            {/* <ImageAppStore /> */}
+
+            <div>
+              <ImageAppStore />
+            </div>
           </div>
         </div>
-        <div className="col-lg-6 my-auto d-none d-lg-block .d-xl-block">
-          <div className="d-none d-lg-block .d-xl-block">
+        <div className="col-lg-6 my-auto d-none d-lg-block d-xl-block">
+          <div className="d-none d-lg-block d-xl-block">
             <Image />
           </div>
         </div>
